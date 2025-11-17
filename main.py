820125ecd40851253d26d3726a8c289fa67df38e
runber777 <68187626+runber777@users.noreply.github.com>
2025-11-17 19:16:37 +0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((900, 600))
+pygame.display.set_caption("BlackJack")
+#icon = pygame.image.load("")
+#pygame.display.set_icon(icon)
+
+running = True
+while running:
+
+    screen.fill((45, 171, 43))
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
