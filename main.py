774d886814b553fd06d68873c1dc2cb20e480a63
runber777 <68187626+runber777@users.noreply.github.com>
2025-11-19 18:31:44 +0300

    win and loose

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import pygame as pg
 import random
+from mechanics import Card, Deck, Player, Dealer
 
-class Card:
-    def __init__(self, value, suit):
-        self.value = value
-        self.suit = suit
-        self.image = pg.image.load(f'karti/karti/{suit}{value}.png')
-        # Scale the image if needed to match your back card size
-        self.image = pg.transform.scale(self.image, (150, 200))
-
-
-class Deck:
-    def __init__(self):
-        self.cards = self.generate_deck()
-        random.shuffle(self.cards)  # Shuffle the deck
-
-    def generate_deck(self):
-        suits = ['chervi', 'bubi', 'kresti', 'piki']
-        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '1']
-        return [Card(value, suit) for suit in suits for value in values]
-
-    def draw_card(self):
-        if self.cards:
-            return self.cards.pop()
-        return None
+# class Card:
+#     def __init__(self, value, suit):
+#         self.value = value
+#         self.suit = suit
+#         self.image = pg.image.load(f'karti/karti/{suit}{value}.png')
+#         # Scale the image if needed to match your back card size
+#         self.image = pg.transform.scale(self.image, (150, 200))
+#
+#
+# class Deck:
+#     def __init__(self):
+#         self.cards = self.generate_deck()
+#         random.shuffle(self.cards)  # Shuffle the deck
+#
+#     def generate_deck(self):
+#         suits = ['chervi', 'bubi', 'kresti', 'piki']
+#         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '1']
+#         return [Card(value, suit) for suit in suits for value in values]
+#
+#     def draw_card(self):
+#         if self.cards:
+#             return self.cards.pop()
+#         return None
 
 pg.init()
 screen = pg.display.set_mode((1200, 600))
@@ -36,51 +37,63 @@ back = pg.transform.scale(back2, (150, 200))
 back_rect = back.get_rect(topleft=(1000, 200))
 
 f1 = pg.font.Font(None, 30)
+f2 = pg.font.Font(None, 72)
 text = f1.render('Конец хода', True, (222,111,160), (143,190,143))
 text_rect = text.get_rect(topleft=(1015, 150))
 
-deck = Deck()
-dealer_cards = []
-player_cards = []
+def draw_message(screen, message):
+    text_surf2 = f2.render(message, True, (255, 0, 0))  # красный цвет
+    text_rect2 = text_surf2.get_rect(center=(600, 300))
+    screen.blit(text_surf2, text_rect2)
 
+deck = Deck()
+player = Player()
+dealer = Dealer()
+
+game_over = False
+message = ""
 running = True
 while running:
     screen.fill((140, 200, 130))
     screen.blit(back, back_rect)
     screen.blit(text, text_rect)
 
-    for card_obj, x, y in dealer_cards:
+    for card_obj, x, y in dealer.hand:
         screen.blit(card_obj.image, (x, y))
 
-    for card_obj, x, y in player_cards:
+    for card_obj, x, y in player.hand:
         screen.blit(card_obj.image, (x, y))
-
+    if message:
+        draw_message(screen, message)
     pg.display.update()
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-        elif event.type == pg.MOUSEBUTTONDOWN:
+        elif event.type == pg.MOUSEBUTTONDOWN and not game_over:
             if event.button == 1:
                 pos = pg.mouse.get_pos()
                 if back_rect.collidepoint(pos):
-                    card = deck.draw_card()
-                    if card:
-                        x = 20 + (len(player_cards) * (100+30))
-                        y = 410
-                        player_cards.append((card, x, y))
-
+                    player.add_card(deck.deal_card())
+                    if player.get_score() > 21:
+                        message = "ВЫ ПРОИГРАЛИ"
+                        game_over = True
                 elif text_rect.collidepoint(pos):
-                    cards_to_deal = random.randint(2, 5)
-                    for i in range(cards_to_deal):
-                        card = deck.draw_card()
-                        if card:
-                            x = 20 + (len(dealer_cards) * (100 + 30))
-                            y = 10
-                            dealer_cards.append((card, x, y))
+                    while dealer.should_hit():
+                        dealer.add_card(deck.deal_card(), y=10)
+                        if dealer.get_score() >= player.get_score():
+                            break
+                    if player.get_score() > dealer.get_score():
+                        message = "ПОБЕДА"
+                    elif player.get_score() == dealer.get_score():
+                        message = "НИЧЬЯ"
+                    else:
+                        message = "ВЫ ПРОИГРАЛИ"
+                    game_over = True
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
 pg.quit()
+
+
+print(player.hand)
+print(player.get_score())
--- mechanics.py
+++ mechanics.py
@@ -1,4 +1,6 @@
-import pygame
+import pygame as pg
+import random
+
 
 class Deck:
     def __init__(self):
@@ -11,7 +13,6 @@ class Deck:
         return [Card(value, suit) for suit in suits for value in values]
 
     def shuffle(self):
-        import random
         random.shuffle(self.cards)
 
     def deal_card(self):
@@ -22,23 +23,26 @@ class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
-        self.image = pygame.image.load(f'karti/karti/{suit}{value}.png')
-
+        self.image = pg.image.load(f'karti/karti/{suit}{value}.png')
+        # Scale the image if needed to match your back card size
+        self.image = pg.transform.scale(self.image, (150, 200))
     def __repr__(self):
         return f"{self.suit}{self.value}"
 
 class Player:
     def __init__(self):
         self.hand = []
+        self.positions = []
 
-    def add_card(self, card):
-        self.hand.append(card)
+    def add_card(self, card, y=410):
+        self.hand.append((card, 20 + (len(self.hand)) * 130, y))
+        #self.positions.append((20 + (len(self.hand)) * 130, 10))
 
     def get_score(self):
         score = 0
         aces = 0
 
-        for card in self.hand:
+        for card, x, y in self.hand:
             if card.value in ['11', '12', '13']:
                 score += 10
             elif card.value == "1":
@@ -50,23 +54,27 @@ class Player:
             score -= 10
             aces -= 1
         return score
+
+
+
 class Dealer(Player):
     def should_hit(self):
         return self.get_score() < 17
 
 
 
-deck = Deck()
-player = Player()
-dealer = Player()
 
-print(deck.cards)
-player.add_card(deck.deal_card())
-player.add_card(deck.deal_card())
-
-
-print(player.hand)
-print(player.get_score())
-
-while dealer.should_hit():
-    dealer.add_card(deck.deal_card())
+# deck = Deck()
+# player = Player()
+# dealer = Dealer()
+#
+# print(deck.cards)
+# player.add_card(deck.deal_card())
+# player.add_card(deck.deal_card())
+#
+#
+# print(player.hand)
+# print(player.get_score())
+#
+# while dealer.should_hit():
+#     dealer.add_card(deck.deal_card())
