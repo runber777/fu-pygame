672c8a10ea0921953f0ff22f726a77712908eeef
yanan-o7 <ydyutina@gmail.com>
2025-11-19 14:35:25 +0300

    ишиг

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-import pygame
+import pygame as pg
 
-pygame.init()
-screen = pygame.display.set_mode((900, 600))
-pygame.display.set_caption("BlackJack")
-#icon = pygame.image.load("")
-#pygame.display.set_icon(icon)
+pg.init()
+screen = pg.display.set_mode((900, 600))
+pg.display.set_caption("BlackJack")
+icon = pg.image.load("karti/karti/icon.jpg")
+pg.display.set_icon(icon)
+
+back2 = pg.image.load("karti/karti/backside.jpg")
+back = pg.transform.scale(back2, (150, 200))
+back_rect = back.get_rect(topleft=(700, 200))
+
+tets = pg.image.load("karti/karti/bubi1.png")
+card_clicked = False
 
 running = True
 while running:
-
     screen.fill((140, 200, 130))
-    pygame.display.update()
+    screen.blit(back, (700, 200))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    if card_clicked:
+        screen.blit(tets, (400, 400))
+
+    screen.blit(tets, (400, 50))
+    pg.display.update()
+
+    mouse_buttons = pg.mouse.get_pressed()
+    pos = pg.mouse.get_pos()
+
+    if mouse_buttons[0] and back_rect.collidepoint(pos):
+        card_clicked = True
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-            pygame.quit()
-# bubu
+pg.quit()
